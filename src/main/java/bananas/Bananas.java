e2a2209850481f26b1dde6a5895d8209c64d8f4f
44jovi <kwong.jovi@gmail.com>
2023-03-17 10:34:46 +0000

    Update file

--- src/main/java/bananas/Bananas.java
+++ src/main/java/bananas/Bananas.java
@@ -2,8 +2,6 @@ package bananas;
 
 public class Bananas {
 	public static void main() {
-		System.out.println("Bananas!");
-		System.out.println("Are!");
-		System.out.println("Great!");
+		System.out.println("Bananas are great!");
 	}
 }
