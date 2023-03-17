953c758d7aa7d60e6b28c881f13db0f7d8c2303c
44jovi <kwong.jovi@gmail.com>
2023-03-17 12:53:26 +0000

    First test

--- src/test/java/bananas/ReverseFactorialTest.java
+++ src/test/java/bananas/ReverseFactorialTest.java
@@ -1,14 +1,17 @@
 package bananas;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class ReverseFactorialTest {
 
+	private ReverseFactorial rf = new ReverseFactorial();
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetResult() {
+		assertEquals(3628800, this.rf.getResult(120));
+//		fail("Not yet implemented");
 	}
 
 }
