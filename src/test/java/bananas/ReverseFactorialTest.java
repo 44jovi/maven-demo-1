79879483d387765679f46ece43a6a2a7807b8d9e
44jovi <kwong.jovi@gmail.com>
2023-03-17 15:55:58 +0000

    Add and pass "Challenge Input" test cases

--- src/test/java/bananas/ReverseFactorialTest.java
+++ src/test/java/bananas/ReverseFactorialTest.java
@@ -18,4 +18,24 @@ class ReverseFactorialTest {
 		assertEquals("NONE", this.rf.getResult(150));
 	}
 
+	@Test
+	void test3628800() {
+		assertEquals("10!", this.rf.getResult(3628800));
+	}
+
+	@Test
+	void test479001600() {
+		assertEquals("12!", this.rf.getResult(479001600));
+	}
+
+	@Test
+	void test6() {
+		assertEquals("3!", this.rf.getResult(6));
+	}
+
+	@Test
+	void test18() {
+		assertEquals("NONE", this.rf.getResult(18));
+	}
+
 }
